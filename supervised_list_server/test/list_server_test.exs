c42ed7a2276f47ec9c2678527b3ff35476dbea8c
Rohan Orton <rohan.orton@gmail.com>
2016-07-26 22:20:25 +0100

    Supervisors

---
+++ supervised_list_server/test/list_server_test.exs
@@ -0,0 +1,24 @@
+defmodule ListServerTest do
+  use ExUnit.Case, async: true
+
+  setup do
+    ListServer.start_link
+    ListServer.clear
+  end
+
+  test "starts out empty" do
+    assert ListServer.items == []
+  end
+
+  test "can add item to list" do
+    ListServer.add "book"
+    assert ListServer.items == ["book"]
+  end
+
+  test "can remove item from list" do
+    ListServer.add "book"
+    ListServer.add "paperclip"
+    ListServer.remove "book"
+    assert ListServer.items == ["paperclip"]
+  end
+end
